f051edb691d53d498d2ec3b51a08052f2a4f16c5
24-023 Shofiatul Mahmudah <shofiatulmahmudah4@gmail.com>
2025-10-12 19:45:29 +0700

    Delete modul3 directory

--- modul3/AMAA2.3.php
+++
@@ -1,8 +0,0 @@
-<?php
-$buahAsosiatif = array("a" => "apel", "b" => "mangga", "c" => "pisang");
-$nilaiBuah = array_values($buahAsosiatif);
-
-echo "<b>3. Hasil array_values():</b><br>";
-print_r($nilaiBuah);
-echo "<br><br>";
-?>
--- modul3/Asosiatif(AIAA)3.php
+++
@@ -1,4 +0,0 @@
-<?php
-$height = array("Andy"=>"176", "Barry"=>"165", "Charlie"=>"170");
-echo "Andy is " . $height['Andy'] . " cm tall.";
-?>
